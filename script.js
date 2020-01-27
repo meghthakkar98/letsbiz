45c98495c2a0a10eca4e07b4c74fb26f6bb10e64
Megh Thakkar <meghthakkar98@gmail.com>
2020-01-28 01:43:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ $(document).ready(function(){
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1000, function(){
+      }, 1000,function(){
         window.location.hash = hash;
       });
     } 
   });
-});	
+});
